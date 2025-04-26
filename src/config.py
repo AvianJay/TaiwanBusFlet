cea3c0db5317b5ed58f66121f13f3efb372ae60e
AvianJay <jay20090728@gmail.com>
2025-04-26 17:48:55 +0800

    update

--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
-# some global variables
+import os
+import taiwanbus
 
+# some global variables
 current_bus = None
+
+platform = os.getenv("FLET_PLATFORM")
+datadir = os.getenv("FLET_APP_STORAGE_DATA")
--- src/viewbus.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-import taiwanbus
-import asyncio
-
-def main(page: ft.Page):
-    page.adaptive = True
-
-    page.add(ft.Text("這是公車資訊頁面"))
