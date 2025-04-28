10a7baa9b9ed5c18980bc04194f977749dc49351
AvianJay <jay20090728@gmail.com>
2025-04-28 22:29:57 +0800

    add config file[WIP]

--- src/config.py
+++ src/config.py
@@ -1,12 +1,35 @@
 import os
+import json
 import taiwanbus
+from importlib.metadata import version
+
+# info
+app_version = "0.0.1"
+config_version = 1
+taiwanbus_version = version("taiwanbus")
 
 # some global variables
 current_bus = None
 bus_update_time = 10  # seconds
 
 platform = os.getenv("FLET_PLATFORM")
-datadir = os.getenv("FLET_APP_STORAGE_DATA")
+datadir = os.getenv("FLET_APP_STORAGE_DATA", ".")
 taiwanbus.update_database_dir(datadir)
+# taiwanbus bug, will be fixed in 0.1.0
 taiwanbus.home = os.path.join(datadir, ".taiwanbus")
-print(datadir)
+
+# config
+default_config = {
+    "config_version": config_version,
+    "provider": "twn",
+    "bus_update_time": 10,
+}
+config_path = os.path.join(datadir, "config.json")
+config = None
+
+if os.path.exists(config_path):
+    config = json.load(open(config_path, "r"))
+    # Todo: verify
+else:
+    config = default_config.copy()
+    json.dump(config, open(config_path, "w"))
