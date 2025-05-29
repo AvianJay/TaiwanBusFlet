e526e522b05dcd3e652a3f69f81dac688da379fb
AvianJay <jay20090728@gmail.com>
2025-05-30 00:53:38 +0800

    flet dont have kivy things

--- src/android.py
+++ src/android.py
@@ -4,28 +4,29 @@ from jnius import autoclass, cast
 
 def get_network_status():
     from multiplatform import NetworkStatus
-    PythonActivity = autoclass('org.kivy.android.PythonActivity')
-    Context = autoclass('android.content.Context')
-    activity = PythonActivity.mActivity
+    return NetworkStatus.UNKNOWN
+    # PythonActivity = autoclass('org.kivy.android.PythonActivity')
+    # Context = autoclass('android.content.Context')
+    # activity = PythonActivity.mActivity
 
-    connectivity_service = activity.getSystemService(Context.CONNECTIVITY_SERVICE)
-    ConnectivityManager = autoclass('android.net.ConnectivityManager')
-    NetworkCapabilities = autoclass('android.net.NetworkCapabilities')
-    Build = autoclass('android.os.Build')
+    # connectivity_service = activity.getSystemService(Context.CONNECTIVITY_SERVICE)
+    # ConnectivityManager = autoclass('android.net.ConnectivityManager')
+    # NetworkCapabilities = autoclass('android.net.NetworkCapabilities')
+    # Build = autoclass('android.os.Build')
 
-    if Build.VERSION.SDK_INT >= 23:
-        network = connectivity_service.getActiveNetwork()
-        if network is None:
-            return NetworkStatus.NO_NETWORK
-        capabilities = connectivity_service.getNetworkCapabilities(network)
+    # if Build.VERSION.SDK_INT >= 23:
+    #     network = connectivity_service.getActiveNetwork()
+    #     if network is None:
+    #         return NetworkStatus.NO_NETWORK
+    #     capabilities = connectivity_service.getNetworkCapabilities(network)
 
-        if capabilities is None:
-            return NetworkStatus.FAILED
-        elif capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI):
-            return NetworkStatus.WIFI
-        elif capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR):
-            return NetworkStatus.CELLULAR
-        else:
-            return NetworkStatus.OTHER
-    else:
-        return NetworkStatus.UNKNOWN
+    #     if capabilities is None:
+    #         return NetworkStatus.FAILED
+    #     elif capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI):
+    #         return NetworkStatus.WIFI
+    #     elif capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR):
+    #         return NetworkStatus.CELLULAR
+    #     else:
+    #         return NetworkStatus.OTHER
+    # else:
+    #     return NetworkStatus.UNKNOWN
