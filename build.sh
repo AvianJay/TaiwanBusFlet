354e1bc9ea677f0c4e60fcae8650a40954b56316
AvianJay <jay20090728@gmail.com>
2025-06-08 06:03:20 +0800

    something

---
+++ build.sh
@@ -0,0 +1,2 @@
+flutter pub get
+dart run --suppress-analytics serious_python:main package app/src/ --platform Android -r flet-geolocator==0.1.0 -r flet-permission-handler==0.1.0 -r flet==0.27.6 -r "requests>2.26.0" -r taiwanbus==0.0.9 -r pyjnius --exclude build --compile-app --compile-packages --cleanup-packages
